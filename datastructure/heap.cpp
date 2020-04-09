ec25abb1b9eb5b070ebc06f61b35acf28657a3cf
Rakesh Didwania <rakesh.didwania@gmail.com>
2020-04-08 21:06:39 -0500

    Simplified heap.

--- datastructure/heap.cpp
+++ datastructure/heap.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 template<typename T>
-class pq
+class heap
 {
 private:
 	vector<T> data;
@@ -13,16 +13,9 @@ private:
 	int right_child(int i) const { return 2 * i + 2; }
 	int parent(int i) const { return (i - 1) / 2; }
 
-public:
-	pq() {}
-	~pq() {}
-
-	void put(const T &val)
+	void swim(size_t i)
 	{
-		data.push_back(val);
-		size_t i = data.size() - 1;
-
-		while (i != 0) {
+		while (i > 0) {
 			size_t p = parent(i);
 			if (data[i] < data[p])
 				swap(data[i], data[p]);
@@ -30,27 +23,22 @@ public:
 		}
 	}
 
-	T get()
+	void sink(size_t i)
 	{
-		T val = data[0];
-		data[0] = data.back();
-		data.pop_back();
-
-		size_t c, c1, c2;
-		size_t i = 0;
+		size_t c, l, r;
 
 		while (i < data.size()) {
-			c1 = left_child(i);
-			c2 = right_child(i);
+			l = left_child(i);
+			r = right_child(i);
 
-			if (c1 >= data.size())
-				c = c2;
-			else if (c2 >= data.size())
-				c = c1;
-			else if (data[c1] < data[c2])
-				c = c1;
+			if (l >= data.size())
+				c = r;
+			else if (r >= data.size())
+				c = l;
+			else if (data[l] < data[r])
+				c = l;
 			else
-				c = c2;
+				c = r;
 
 			if ((c < data.size()) && (data[c] < data[i])) {
 				swap(data[c], data[i]);
@@ -59,8 +47,25 @@ public:
 				break;
 			}
 		}
+	}
 
-		return val;
+public:
+	heap() {}
+	~heap() {}
+
+	void put(const T &val)
+	{
+		data.push_back(val);
+		swim(data.size() - 1);
+	}
+
+	T get()
+	{
+		T top = data[0];
+		data[0] = data.back();
+		data.pop_back();
+		sink(0);
+		return top;
 	}
 
 	bool empty() const
@@ -82,20 +87,19 @@ public:
 int
 main()
 {
-	pq<int> q;
+	heap<int> h;
 
-	q.put(5);
-	q.put(4);
-	q.put(2);
-	q.put(6);
-	q.put(1);
-	q.put(3);
-	q.put(0);
+	h.put(15);
+	h.put(27);
+	h.put(19);
+	h.put(33);
+	h.put(47);
+	h.put(23);
 
-	q.dump();
+	h.dump();
 
-	while (!q.empty())
-		cout << q.get() << endl;
+	while (!h.empty())
+		cout << h.get() << endl;
 	
 	return 0;
 }
